df33030c0cef16153981f270b06324a71281959e
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-02-16 16:30:40 +0900

    build : AWS RDS DB연결, yml 수정

--- src/main/java/com/aloharoombackend/AloharoomBackendApplication.java
+++ src/main/java/com/aloharoombackend/AloharoomBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AloharoomBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AloharoomBackendApplication.class, args);
 	}
