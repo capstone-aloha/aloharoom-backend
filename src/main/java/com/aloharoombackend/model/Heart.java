e55199a2367368b5a256f2d950467863196253fe
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-03-09 11:15:18 +0900

    chore : AddHeartDto 구문 삭제

--- src/main/java/com/aloharoombackend/model/Heart.java
+++ src/main/java/com/aloharoombackend/model/Heart.java
@@ -1,13 +1,11 @@
 package com.aloharoombackend.model;
 
-import com.aloharoombackend.dto.AddHeartDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
-import javax.persistence.Table;
 
 @Entity
 @Getter
@@ -25,8 +23,4 @@ public class Heart {
         this.userId = userId;
     }
 
-    public Heart(AddHeartDto addLikeDto) {
-        this.boardId = addLikeDto.getBoardId();
-        this.userId = addLikeDto.getUserId();
-    }
 }
