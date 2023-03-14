32ab9b9c2473f34f3aa26a3034baf9702d6c9549
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-03-14 19:11:03 +0900

    feat : 한 번에 유저 조회(MyPageDto 생성)

---
+++ src/main/java/com/aloharoombackend/dto/MyPageDto.java
@@ -0,0 +1,52 @@
+package com.aloharoombackend.dto;
+
+import com.aloharoombackend.model.LikeProduct;
+import com.aloharoombackend.model.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+public class MyPageDto {
+
+    //user
+    private Long signupId;
+    private String username;
+    private String password;
+    private String nickname;
+    //private String name;
+    private Integer age;
+    private String gender;
+    private String role;
+    private String tendency;
+
+    //likeHash
+    private List<String> likeHashtags;
+
+    //likeProduct
+    private List<String> likeProducts;
+
+    //myHash
+    private List<String> myHashtags;
+
+    //myProduct
+    private List<String> myProducts;
+
+    public MyPageDto(User user) {
+        this.signupId = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.age = user.getAge();
+        this.gender = user.getGender();
+        this.role = user.getRole();
+        this.tendency = user.getTendency();
+        this.likeProducts = user.getLikeProducts().stream().map(likeProduct -> likeProduct.getName()).collect(Collectors.toList());
+        this.likeHashtags = user.getLikeHashtags().stream().map(likeHashtag -> likeHashtag.getHash()).collect(Collectors.toList());
+        this.myProducts = user.getMyProducts().stream().map(myProduct -> myProduct.getName()).collect(Collectors.toList());
+        this.myHashtags = user.getMyHashtags().stream().map(myHashtag -> myHashtag.getHash()).collect(Collectors.toList());
+    }
+}
