382ea60786d00e6a3c9e90d4d1e07eb5248c6915
HyeongMin Kim <fkgnssla@naver.com>
2023-03-17 19:55:16 +0900

    modify : BaseEntity @Setter 추가

--- src/main/java/com/aloharoombackend/model/BaseEntity.java
+++ src/main/java/com/aloharoombackend/model/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.aloharoombackend.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@Setter
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
