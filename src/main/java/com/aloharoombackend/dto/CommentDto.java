566189021df2dd34dc01f11c20a75824bf936513
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-04-03 15:59:24 +0900

    chore : CommentDto 수정

--- src/main/java/com/aloharoombackend/dto/CommentDto.java
+++ src/main/java/com/aloharoombackend/dto/CommentDto.java
@@ -4,16 +4,25 @@ import com.aloharoombackend.model.HomeComment;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class CommentDto {
+    private Long id;
     private String nickname;
     private String content;
+    private LocalDateTime createdDate;
+    private List<CommentDto> commentDtos = new ArrayList<>();
     //추후 시간 추가
 
     public CommentDto(HomeComment homeComment) {
+        this.id = homeComment.getId();
         this.nickname = homeComment.getUser().getNickname();
         this.content = homeComment.getContent();
+        this.createdDate = homeComment.getCreatedDate();
     }
 
 }
