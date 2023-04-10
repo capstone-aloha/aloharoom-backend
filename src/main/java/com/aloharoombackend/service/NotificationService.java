bb98d9787345592b9f56f00630d9f815f8aae615
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-04-10 12:06:41 +0900

    feat : 알림 내림차순(시간) 정렬

--- src/main/java/com/aloharoombackend/service/NotificationService.java
+++ src/main/java/com/aloharoombackend/service/NotificationService.java
@@ -6,6 +6,8 @@ import com.aloharoombackend.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +23,15 @@ public class NotificationService {
 
     public List<NotificationDto> getNotification(Long userId) {
         List<Notification> notifications = notificationRepository.findAllByUserId(userId);
+        //알림 내림차순(시간) 정렬
+        Collections.sort(notifications, new Comparator<Notification>() {
+            @Override
+            public int compare(Notification o1, Notification o2) {
+                if(o1.getCreatedDate().isBefore(o2.getCreatedDate())) return 1;
+                else if(o1.getCreatedDate().isAfter(o2.getCreatedDate())) return -1;
+                else return 0;
+            }
+        });
         return notifications.stream().map(notification -> new NotificationDto(notification))
                 .collect(Collectors.toList());
     }
