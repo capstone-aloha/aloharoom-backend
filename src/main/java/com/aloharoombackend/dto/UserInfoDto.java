6481cd9b9bea5327f8916d947f2bf2b8a33d8987
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-04-10 13:04:02 +0900

    chore : UserInfoDto 값 수정

--- src/main/java/com/aloharoombackend/dto/UserInfoDto.java
+++ src/main/java/com/aloharoombackend/dto/UserInfoDto.java
@@ -1,5 +1,7 @@
 package com.aloharoombackend.dto;
 
+import com.aloharoombackend.model.MyHashtag;
+import com.aloharoombackend.model.MyProduct;
 import com.aloharoombackend.model.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +12,13 @@ import java.util.stream.Collectors;
 @Data
 @NoArgsConstructor
 public class UserInfoDto {
-    private List<String> likeHashtags;
-    private List<String> likeProducts;
-//    private List<String> myHashtags;
-//    private List<String> myProducts;
+//    private List<String> likeHashtags;
+//    private List<String> likeProducts;
+    private List<String> myHashtags;
+    private List<String> myProducts;
 
     public UserInfoDto(User user) {
-        this.likeHashtags = user.getLikeHashtags().stream().map(likeHashtag -> likeHashtag.getHash()).collect(Collectors.toList());
-        this.likeProducts = user.getLikeProducts().stream().map(likeProduct -> likeProduct.getName()).collect(Collectors.toList());
+        this.myHashtags = user.getMyHashtags().stream().map(MyHashtag::getHash).collect(Collectors.toList());
+        this.myProducts = user.getMyProducts().stream().map(MyProduct::getName).collect(Collectors.toList());
     }
 }
