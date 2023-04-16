c59de0f778e1ac8746b8140f9041ce90c99daf1a
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-04-16 18:13:40 +0900

    feat : 내가 쓴 방, 커뮤니티 모두 조회
    service에서 비즈니스 로직 해결

--- src/main/java/com/aloharoombackend/repository/BoardRepository.java
+++ src/main/java/com/aloharoombackend/repository/BoardRepository.java
@@ -3,7 +3,6 @@ package com.aloharoombackend.repository;
 import com.aloharoombackend.model.Board;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +13,6 @@ public interface BoardRepository extends JpaRepository<Board, Long>, BoardReposi
     @Override
     @EntityGraph(attributePaths = {"user"})
     List<Board> findAll();
+
+    List<Board> findAllByUserId(Long userId);
 }
