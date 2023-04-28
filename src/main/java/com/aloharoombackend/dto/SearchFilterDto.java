247fb83d2d360df47ba55da12ec0a7ea95d9af36
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-04-28 13:13:50 +0900

    chore : 방 필터링 SearchFilterDto 형식 변경

--- src/main/java/com/aloharoombackend/dto/SearchFilterDto.java
+++ src/main/java/com/aloharoombackend/dto/SearchFilterDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,21 +15,12 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class SearchFilterDto {
 
-    private Integer minAge;
-    private Integer maxAge;
-
     private String gender;
-
-    private Integer minFlat; //최소 평수
-    private Integer maxFlat; //최대 평수
-
     private Integer roomCount; //방 갯수
-
     private String homeType; //아파트, 빌라, 주택
-
-    private Integer minRent; //최소 월 가격
-    private Integer maxRent; //최대 월 가격
-
+    private List<Integer> ageRange; //원하는 나이 범위
+    private List<Integer> flatRange; //원하는 평수 범위
+    private List<Integer> rentRange; //원하는 월세 범위
     private List<String> likeHashtags;
 
     public void setUser(User user) {
@@ -39,14 +31,11 @@ public class SearchFilterDto {
     //test용
     public SearchFilterDto(Integer minAge, Integer maxAge, String gender, List<String> likeHashtags, Integer minFlat, Integer maxFlat,
                            Integer minRent, Integer maxRent, Integer roomCount, String homeType) {
-        this.minAge = minAge;
-        this.maxAge = maxAge;
+        this.ageRange = Arrays.asList(minAge, maxAge);
         this.gender = gender;
         this.likeHashtags = likeHashtags;
-        this.minFlat = minFlat;
-        this.maxFlat = maxFlat;
-        this.minRent = minRent;
-        this.maxRent = maxRent;
+        this.flatRange = Arrays.asList(minFlat, maxFlat);
+        this.rentRange = Arrays.asList(minRent, maxRent);
         this.roomCount = roomCount;
         this.homeType = homeType;
     }
