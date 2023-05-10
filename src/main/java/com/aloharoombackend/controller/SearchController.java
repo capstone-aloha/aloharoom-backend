42803a8870732194886e02f36ff7c1e924638f18
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-05-10 16:22:08 +0900

    chore : 방 필터링 메소드 인수 변경

--- src/main/java/com/aloharoombackend/controller/SearchController.java
+++ src/main/java/com/aloharoombackend/controller/SearchController.java
@@ -3,16 +3,12 @@ package com.aloharoombackend.controller;
 import com.aloharoombackend.auth.PrincipalDetails;
 import com.aloharoombackend.dto.BoardAllDto;
 import com.aloharoombackend.dto.SearchFilterDto;
-import com.aloharoombackend.model.User;
 import com.aloharoombackend.service.BoardService;
 import com.aloharoombackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +20,17 @@ public class SearchController {
     private final BoardService boardService;
 
     @GetMapping
-    public ResponseEntity<List<BoardAllDto>> searchFilter(@RequestBody SearchFilterDto searchFilterDto,
+    public ResponseEntity<List<BoardAllDto>> searchFilter(
+                                                          @RequestParam String gender,
+                                                          @RequestParam Integer roomCount,
+                                                          @RequestParam String homeType,
+                                                          @RequestParam List<Integer> ageRange,
+                                                          @RequestParam List<Integer> flatRange,
+                                                          @RequestParam List<Integer> rentRange,
+                                                          @RequestParam List<String> likeHashtags,
                                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        SearchFilterDto searchFilterDto = new SearchFilterDto(gender, roomCount, homeType, ageRange, flatRange, rentRange, likeHashtags);
+//        System.out.println(searchFilterDto);
         return ResponseEntity.ok(boardService.searchFilter(principalDetails.getUser().getId(), searchFilterDto));
     }
 }
