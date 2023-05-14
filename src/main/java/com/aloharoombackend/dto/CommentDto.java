95d87a7c06cb5d3da3bcdd9e148b8d1e9f31c06b
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-05-14 11:24:40 +0900

    feat : 댓글 조회 시 프로필 이미지 추가

--- src/main/java/com/aloharoombackend/dto/CommentDto.java
+++ src/main/java/com/aloharoombackend/dto/CommentDto.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class CommentDto {
     private Long commentId;
     private Long userId;
+    private String profileUrl;
     private String nickname;
     private String content;
     private String createdDate;
@@ -20,6 +21,7 @@ public class CommentDto {
     public CommentDto(Comment comment) {
         this.commentId = comment.getId();
         this.userId = comment.getUser().getId();
+        this.profileUrl = comment.getUser().getProfileUrl();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
         //2023.04.05 16:23
