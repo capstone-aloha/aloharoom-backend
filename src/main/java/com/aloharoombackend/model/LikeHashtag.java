bb48a4fe157d589c7bb2c8e8e23e0e3c1509071b
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-05-23 22:59:41 +0900

    fix : 회원 수정 시 모든 해시태그 없을 때 에러 수정

--- src/main/java/com/aloharoombackend/model/LikeHashtag.java
+++ src/main/java/com/aloharoombackend/model/LikeHashtag.java
@@ -13,8 +13,8 @@ public class LikeHashtag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     private String hash;
